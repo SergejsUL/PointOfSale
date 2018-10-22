14dff2648799fd65c480aeb168e97f60b7cf2d11
SergejsUL <39090869+SergejsUL@users.noreply.github.com>
2018-10-22 22:23:43 +0100

    context menu created

--- PointOfSale/app/src/main/java/ie/ul/serge/pointofsale/MainActivity.java
+++ PointOfSale/app/src/main/java/ie/ul/serge/pointofsale/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,15 +18,16 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mNameTextView,mQuantityTextView,mDateTextView;
-    private Item mCurrentItem,mDeletedItem;
-    private ArrayList <Item> mItems = new ArrayList<>();
+    private TextView mNameTextView, mQuantityTextView, mDateTextView;
+    private Item mCurrentItem, mDeletedItem;
+    private ArrayList<Item> mItems = new ArrayList<>();
 
 
     @Override
@@ -33,14 +35,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNameTextView= findViewById(R.id.name_text);
-        mQuantityTextView=findViewById(R.id.quantity_text);
-        mDateTextView=findViewById(R.id.date_text);
+        mNameTextView = findViewById(R.id.name_text);
+        mQuantityTextView = findViewById(R.id.quantity_text);
+        mDateTextView = findViewById(R.id.date_text);
         mItems.add(new Item("boots", 20, new GregorianCalendar()));
         mItems.add(new Item("shorts", 2, new GregorianCalendar()));
         mItems.add(new Item("sandals", 5, new GregorianCalendar()));
         mItems.add(new Item("shades", 8, new GregorianCalendar()));
-
+        registerForContextMenu(mNameTextView);
 
 
         //boilerplate content
@@ -63,8 +65,8 @@ public class MainActivity extends AppCompatActivity {
         //builder.setTitle("My Title");
         //builder.setMessage("Hello World My Message!");
         //builder.setPositiveButton("OK",null); -- but you can make dialogues from xml.
-       // builder.setTitle(R.string.add_item);
-        View view = getLayoutInflater().inflate(R.layout.dialog_add,null,false);
+        // builder.setTitle(R.string.add_item);
+        View view = getLayoutInflater().inflate(R.layout.dialog_add, null, false);
         builder.setView(view);
         //get values from the input fields and store them. They have to be final as they will be used in the inner class.
         final EditText nameEditText = view.findViewById(R.id.edit_name);
@@ -75,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         deliveryDateView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                callendar.set(year,month,dayOfMonth);
+                callendar.set(year, month, dayOfMonth);
             }
         });
 
@@ -83,14 +85,14 @@ public class MainActivity extends AppCompatActivity {
         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-            String name = nameEditText.getText().toString();
-            int quantity = Integer.parseInt(quantityEditText.getText().toString());
-            mCurrentItem = new Item(name,quantity,callendar);
-            showCurrentItem();
-            mItems.add(mCurrentItem);
+                String name = nameEditText.getText().toString();
+                int quantity = Integer.parseInt(quantityEditText.getText().toString());
+                mCurrentItem = new Item(name, quantity, callendar);
+                showCurrentItem();
+                mItems.add(mCurrentItem);
             }
         });
-        builder.setNegativeButton(android.R.string.cancel,null);
+        builder.setNegativeButton(android.R.string.cancel, null);
 
         builder.create().show();
 
@@ -98,8 +100,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void showCurrentItem() {
         mNameTextView.setText(mCurrentItem.getName());
-        mQuantityTextView.setText(getString(R.string.quantity_format,mCurrentItem.getQuantity()));
-        mDateTextView.setText(getString(R.string.date_format,mCurrentItem.getDeliveryDateString()));
+        mQuantityTextView.setText(getString(R.string.quantity_format, mCurrentItem.getQuantity()));
+        mDateTextView.setText(getString(R.string.date_format, mCurrentItem.getDeliveryDateString()));
     }
 
     @Override
@@ -113,17 +115,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_reset:
-                mDeletedItem=mCurrentItem;
-                mCurrentItem=new Item();
+                mDeletedItem = mCurrentItem;
+                mCurrentItem = new Item();
                 showCurrentItem();
 
-                Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator_layout),"Item cleared",Snackbar.LENGTH_LONG);
+                Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator_layout), "Item cleared", Snackbar.LENGTH_LONG);
                 snackbar.setAction("UNDO", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        mCurrentItem=  mDeletedItem;
+                        mCurrentItem = mDeletedItem;
                         showCurrentItem();
                     }
                 });
@@ -142,26 +144,45 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             case R.id.action_clear_all:
 
-            clearAllItems();
+                clearAllItems();
 
-            return true;
+                return true;
         }
 
 
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_context,menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_edit:
+                Toast.makeText(this,"TODO: Action Edit",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_remove:
+                Toast.makeText(this,"TODO: Action Remove",Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
     private void clearAllItems() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.remove_items_dialogue_title);
         builder.setMessage(R.string.remove_items_message);
-        builder.setNegativeButton(android.R.string.cancel,null);
+        builder.setNegativeButton(android.R.string.cancel, null);
         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-             mItems = new ArrayList<>();
-             mCurrentItem = new Item();
-             showCurrentItem();
+                mItems.clear();
+                mCurrentItem = new Item();
+                showCurrentItem();
             }
         });
 
@@ -174,21 +195,22 @@ public class MainActivity extends AppCompatActivity {
         builder.setItems(getNames(), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-               mCurrentItem = mItems.get(which);
+                mCurrentItem = mItems.get(which);
                 showCurrentItem();
             }
         });
-        builder.setNegativeButton(android.R.string.cancel,null);
+        builder.setNegativeButton(android.R.string.cancel, null);
 
         builder.create().show();
     }
 
     private String[] getNames() {
-    String [] names = new String [mItems.size()];
+        String[] names = new String[mItems.size()];
 
-    for (int i=0;i<mItems.size();i++){
-        names[i]=mItems.get(i).getName();}
+        for (int i = 0; i < mItems.size(); i++) {
+            names[i] = mItems.get(i).getName();
+        }
 
-    return names;
+        return names;
     }
 }
